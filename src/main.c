e82f8e970cfac2f3b8b3f8287ebef33f45a5f50f
bigsupp <bigsupp@gmail.com>
2015-02-17 03:16:53 +0700

    Add hour to clock face
    Now it has an hour indicator (forget in previous)

--- src/main.c
+++ src/main.c
@@ -4,13 +4,32 @@ static Window *window;
 
 static Layer *path_layer;
 
-static const int HEIGHT_OFFSET = -6;
+static const int HEIGHT_OFFSET = -8;
+
+static int currentHour = 0;
+
+static GPoint clockfaceCenter;
 
 static TextLayer *textMinute;
 static TextLayer *textSecond;
 static TextLayer *textDate;
 
-static const GPathInfo HAND_HOUR_PATH_POINT = {
+static const GPoint *HOUR_POSITION = (GPoint []){
+	{0,-54},
+	{27,-47},
+	{47,-27},
+	{54,0},
+	{47,27},
+	{27,47},
+	{0,54},
+	{-27,47},
+	{-47,27},
+	{-54,0},
+	{-47,-27},
+	{-27,-47}
+};
+
+static const GPathInfo CLOCKFACE_INNER_PATH_POINT = {
   12,
   (GPoint []) {
 		{52,-30},
@@ -28,41 +47,69 @@ static const GPathInfo HAND_HOUR_PATH_POINT = {
 	}
 };
 
-static GPath *handHour_path = NULL;
+static const GPathInfo CLOCKFACE_OUTER_PATH_POINT = {
+  12,
+  (GPoint []) {
+		{54,-31},
+		{31,-54},
+		{-0,-62},
+		{-31,-54},
+		{-54,-31},
+		{-62,0},
+		{-54,31},
+		{-31,54},
+		{0,62},
+		{31,54},
+		{54,31},
+		{62,0}
+	}
+};
 
-static int path_angle = 0;
-static bool outline_mode = true;
+static GPath *clockfaceInner_path = NULL;
+static GPath *clockfaceOuter_path = NULL;
+
+//static int path_angle = 0;
 
 // This is the layer update callback which is called on render updates
 static void path_layer_update_callback(Layer *me, GContext *ctx) {
   (void)me;
 
-  // You can rotate the path before rendering
-  gpath_rotate_to(handHour_path, (TRIG_MAX_ANGLE / 360) * path_angle);
+  //gpath_rotate_to(handHour_path, (TRIG_MAX_ANGLE / 360) * path_angle);
 
-  if (outline_mode) {
-    // draw outline uses the stroke color
-    graphics_context_set_stroke_color(ctx, GColorWhite);
-    gpath_draw_outline(ctx, handHour_path);
-  } else {
-    // draw filled uses the fill color
-    graphics_context_set_fill_color(ctx, GColorWhite);
-    gpath_draw_filled(ctx, handHour_path);
-  }
+  graphics_context_set_fill_color(ctx, GColorWhite);
+	gpath_draw_filled(ctx, clockfaceOuter_path);
+	
+	graphics_context_set_fill_color(ctx, GColorBlack);
+	gpath_draw_filled(ctx, clockfaceInner_path);
+	
+	graphics_context_set_fill_color(ctx, GColorWhite);
+	int hPosX = HOUR_POSITION[currentHour].x+clockfaceCenter.x;
+	int hPosY = HOUR_POSITION[currentHour].y+clockfaceCenter.y;
+	graphics_fill_circle(ctx, GPoint(hPosX,hPosY), 2);
+
 }
 
 static void update_time() {
 	time_t temp = time(NULL);
 	struct tm *tick_time = localtime(&temp);
 	
+	// Hour
+	static char bufferHH[] = "00";
+	strftime(bufferHH,sizeof("00"),"%l",tick_time);
+	currentHour = atoi(bufferHH);
+	if(currentHour==12) currentHour = 0;
+	
+	// Minute
 	static char bufferMM[] = "00";
 	strftime(bufferMM,sizeof("00"),"%M",tick_time);
   text_layer_set_text(textMinute, bufferMM);
 	
+	// Second
 	static char bufferSS[] = "00";
 	strftime(bufferSS,sizeof("00"),"%S",tick_time);
   text_layer_set_text(textSecond, bufferSS);
 	
+	// Date
 	static char bufferDate[16];
 	strftime(bufferDate, sizeof(bufferDate), "%d %b %Y", tick_time);
 	text_layer_set_text(textDate, bufferDate);
@@ -83,12 +130,14 @@ static void init() {
 
   path_layer = layer_create(bounds);
   layer_set_update_proc(path_layer, path_layer_update_callback);
-  // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, path_layer);
 
-  // Pass the corresponding GPathInfo to initialize a GPath
-	handHour_path = gpath_create(&HAND_HOUR_PATH_POINT);
-	gpath_move_to(handHour_path, GPoint(bounds.size.w/2, (bounds.size.h/2)+HEIGHT_OFFSET ));
+	clockfaceCenter = GPoint(bounds.size.w/2, (bounds.size.h/2)+HEIGHT_OFFSET);
+
+	clockfaceOuter_path = gpath_create(&CLOCKFACE_OUTER_PATH_POINT);
+	gpath_move_to(clockfaceOuter_path,clockfaceCenter);
+  clockfaceInner_path = gpath_create(&CLOCKFACE_INNER_PATH_POINT);
+	gpath_move_to(clockfaceInner_path,clockfaceCenter);
 
 	textDate = text_layer_create(GRect(0,142,144,18));
 	text_layer_set_background_color(textDate, GColorClear);
@@ -98,7 +147,7 @@ static void init() {
   text_layer_set_text_alignment(textDate, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(textDate));
 	
-	textSecond = text_layer_create(GRect(77,72+HEIGHT_OFFSET,18,26));
+	textSecond = text_layer_create(GRect(77,71+HEIGHT_OFFSET,18,26));
 	text_layer_set_background_color(textSecond, GColorClear);
 	text_layer_set_text_color(textSecond, GColorWhite);
   text_layer_set_text(textSecond,"00");
@@ -106,7 +155,7 @@ static void init() {
   text_layer_set_text_alignment(textSecond, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(textSecond));
 	
-	textMinute = text_layer_create(GRect(52,65+HEIGHT_OFFSET,24,40));
+	textMinute = text_layer_create(GRect(52,64+HEIGHT_OFFSET,24,40));
 	text_layer_set_background_color(textMinute, GColorClear);
 	text_layer_set_text_color(textMinute, GColorWhite);
   text_layer_set_text(textMinute,"00");
@@ -119,7 +168,8 @@ static void init() {
 }
 
 static void deinit() {
-  gpath_destroy(handHour_path);
+  gpath_destroy(clockfaceOuter_path);
+  gpath_destroy(clockfaceInner_path);
   layer_destroy(path_layer);
 	text_layer_destroy(textMinute);
   window_destroy(window);
